d664b99b9a2336b5a721c819ef2d19a55fead229
petervargaofficial <petervarga@Peters-MacBook-Pro.local>
2014-04-11 07:57:02 -0700

    client almost done, it's however prining also animal.to_s when called to display pets name. weird.

---
+++ client.rb
@@ -0,0 +1,28 @@
+class Client
+
+  attr_accessor :name, :age, :gender, :kids, :pets_number
+
+  def initialize(name, age, gender, kids)
+    @name = name
+    @age = age
+    @gender = gender
+    @kids = kids
+    @pets_number = 0
+    @pets_list = []
+
+  end
+
+  def to_s
+    "#{@name} is a #{@age} year old #{@gender} with #{@kids} kids and #{@pets_number} pets"
+  end
+
+  def display_pets
+    @pets_list.each {|pet| print pet.name}
+  end
+
+# Tiny is a 3 year old male cat that loves \nFranz is a 5 year old male cat that loves ")
+
+end
+
+# bety = Client.new('Beth', 30, 'female', 3)
+# puts bety.to_s
