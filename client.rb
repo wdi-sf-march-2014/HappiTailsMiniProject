c79c6756973e6cac174b1164d75203bfdaa8a97e
Jade <jade.applegate@gmail.com>
2014-04-10 18:53:06 -0700

    phase 1 done

---
+++ client.rb
@@ -0,0 +1,35 @@
+# - Client:
+# 	- A client should have a name.
+# 	- A client should have a number of children.
+#   - A client should have a gender.
+# 	- A client should have an age.
+# 	- A client should have pets.
+
+class Client 
+
+	attr_accessor :name, :age, :gender, :children, :pets
+
+  def initialize(name, age, gender, children)
+    @name = name
+    @age = age
+    @gender = gender
+    @children = children
+    @pets = {}
+  end
+
+ def to_s
+ 	"#{@name} is a #{@age} year old #{@gender} with #{@children} kids and #{@pets.length} pets"
+ end
+
+ def display_pets
+ 	pets = ""
+ 	@pets.each do |k, v|
+ 		pets += (v.to_s + "\n")
+ 	end
+ 		return pets.chomp
+ end
+
+
+
+end
+
