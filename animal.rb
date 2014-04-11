904ecd635979274398351a84ca9692c6104a53d9
Sterling Radcliff <radcliff@riseup.net>
2014-04-10 20:23:38 -0700

    trivial changes, fixed string interpolation in animal.rb, finished up client.rb

--- animal.rb
+++ animal.rb
@@ -1,5 +1,4 @@
 class Animal
-
   attr_accessor :name, :age, :gender, :species, :toys
 
   def initialize(name, age, gender, species)
@@ -8,13 +7,10 @@ class Animal
     @gender = gender
     @species = species
     @toys = []
-
   end
 
-  # When we display the animal using puts or print, the
-  #   to_s method is called to pretty print an Animal
   def to_s
-    "#{name} is a #{age} year old #{gender} #{species} that loves mice, string"
+    "#{name} is a #{age} year old #{gender} #{species} that loves #{toys.join(", ")}"
   end
 end
 
--- client.rb
+++ client.rb
@@ -1,6 +1,6 @@
 class Client < Animal
 
-  attr_accessor :name, :age, :gender, :number_of_children, :pets, :gender
+  attr_accessor :name, :age, :gender, :number_of_children, :pets
 
   def initialize(name, age, gender, number_of_children)   
     @name = name
@@ -11,13 +11,13 @@ class Client < Animal
   end
 
   def to_s
-    "#{@name} is a #{@age} year old #{@gender} with #{@number_of_children} kids and #{@pets.length} pets"   #female hard code?
+    "#{@name} is a #{@age} year old #{@gender} with #{@number_of_children} kids and #{@pets.length} pets"
   end
 
   def display_pets
     pets = ""
     @pets.each do |key, value| 
-      empty_string += (value.to_s + "\n")
+      pets += (value.to_s + "\n")
     end
     return pets.chomp()
     # "#{value.name} is a #{value.age} year old #{value.gender} #{value.species} that loves "
