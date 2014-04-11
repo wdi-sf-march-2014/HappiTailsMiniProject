e9fca61f6e0fac756554e57f6b656bd545dbda25
Githubshanna <shannahutch@gmail.com>
2014-04-10 18:24:10 -0700

    phase 1 commit

---
+++ client.rb
@@ -0,0 +1,36 @@
+class Client
+
+  # Set up accessors and mutators for the attributes of an Animal
+  # attr_accessor sets up both for you
+  attr_accessor :name, :number_of_children, :age, :pets
+
+
+  # Used when creating a new animal.
+  #   Example:
+  #       Animal.new('Timmy', 4, 'male', 'toad')
+  def initialize(name, age, gender, number_of_children)
+        @name = name
+        @age = age
+        @gender = gender
+        @number_of_children = number_of_children
+        @pets ={}
+  end
+    
+  # When we display the animal using puts or print, the
+  #   to_s method is called to pretty print an Animal
+  # def to_s
+     def to_s
+
+        "#{@name} is a #{@age} year old #{@gender} with #{@number_of_children} kids and #{@pets.length} pets"
+      end
+
+    def display_pets
+      pets = ""
+       @pets.each do |k,v| 
+          pets += (v.to_s + "\n")
+        end
+        return pets.chomp
+
+    end
+  # end
+end
