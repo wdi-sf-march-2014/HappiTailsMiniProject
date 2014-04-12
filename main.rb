abc3cc51e79b610fa3413fc4a3724d07ada168e9
daveyk535 <dave@phase2minds.com>
2014-04-12 14:22:07 -0700

    Got basic menu working

--- main.rb
+++ main.rb
@@ -1,11 +1,44 @@
 require 'pry'
-require 'rainbow'
-require_relative 'data'
+#require 'rainbow'
+#require_relative 'data'
 
+puts "Select an option:"
 
-response = #set this equal to something
-while response != 'Q'
-  ##Fill in code here
-  
-  response = #set this equal to something
+puts "Display animals (1)"
+puts "Display clients (2)"
+puts "Create animals (3)"
+puts "Create clients (4)"
+puts "Quit (5)"
+
+input = gets.chomp.to_s
+
+while input != "5"
+
+	case input
+		when "1"
+			puts "Display animals"
+		
+		when "2"
+			puts "Display clients"
+		
+		when "3"
+			puts "Create animals"
+		
+		when "4"
+			puts "Create clients"
+			
+	end
+
+puts "Select an option:"
+
+puts "Display animals (1)"
+puts "Display clients (2)"
+puts "Create animals (3)"
+puts "Create clients (4)"
+puts "Quit (5)"
+
+input = gets.chomp.to_s
+	
 end
+
+
