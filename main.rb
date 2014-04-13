84ce0786d3b97637e32f5bdaba4015ac99678bae
daveyk535 <dave@phase2minds.com>
2014-04-12 22:37:12 -0700

    Create animal working, but without toys

--- main.rb
+++ main.rb
@@ -2,8 +2,9 @@ require 'pry'
 #require 'rainbow'
 require_relative 'data'
 
+puts "\n"
 puts "Select an option:"
-
+puts "\n"
 puts "Display animals (1)"
 puts "Display clients (2)"
 puts "Create animals (3)"
@@ -16,22 +17,42 @@ while input != "5"
 
 	case input
 		when "1"
+			puts "\n"
 			$shelter.display_animals
-			puts $shelter.address
-			# puts "Display animals"
-		when "2"
-			puts $shelter.display_clients
 		
+		when "2"
+			puts "\n"
+			puts $shelter.display_clients
+	
 		when "3"
-			puts "Create animals"
+			puts "Name: "
+			name = gets.chomp
+			puts "Age: "
+			age = gets.chomp
+			puts "Gender: "
+			gender = gets.chomp
+			puts "Species: "
+			species = gets.chomp
+
+			puts "Toys: "
+			toys = gets.chomp
+			
+			#optional checkpoint: puts "Confirm: #{name} - #{age} - #{gender} - #{species}"
+			$shelter.animals[name] = Animal.new(name, age, gender, species)
+		  puts "\n"
+
+		  #optional checkpoint: Print out a newly added animal:
+		  $shelter.animals.each do |k, v|
+				puts "#{v}"
+			end
 		
 		when "4"
-			puts "Create clients"
+			#puts "Create clients"
 			
 	end
 
+puts "\n"
 puts "Select an option:"
-
 puts "Display animals (1)"
 puts "Display clients (2)"
 puts "Create animals (3)"
