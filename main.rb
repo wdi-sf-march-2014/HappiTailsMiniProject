abf5e34e3c99a5fb8f2859afa1c5eff41f977c74
shikoshak <alghaithi82@outlook.com>
2014-04-16 07:10:39 -0700

    the display methods

--- main.rb
+++ main.rb
@@ -1,10 +1,92 @@
-require 'pry'
-require 'rainbow'
+#require 'pry'
+# require 'rainbow'
 require_relative 'data'
 
-response = #set this equal to something
-while response != 'Q'
+puts "Please choose one of the following options:
+		(da) => display all animals
+		(dc) => display all clients
+		(ca) =>  Create an animal
+		(cc) =>  Create a client
+		(aa) => adopt an animal
+		(ra) => return an animal
+		(q)  => quit program"
+
+response = gets.chomp.downcase #set this equal to something
+
+
+while response != 'q'
   ##Fill in code here
-  
-  response = #set this equal to something
+
+	case response
+	when "da"
+		# puts "Enter an animal's name?"
+		# name= gets.chomp
+
+		#display all animals in shelter
+		 $shelter.display_pets
+
+		
+		
+	when "dc"
+		# puts "Enter a client"
+		# client = gets.chomp
+		$shelter.display_clients
+		
+
+	when "ca"
+		puts "Enter a pet name"
+		name = gets.chomp
+		puts "Enter a pet age"
+		age = gets.chomp
+		puts "Enter a pet gender"
+		gender = gets.chomp
+		puts "Enter a pet species"
+		species = gets.chomp
+		
+		$shelter.create_a_animal(name, age, gender, species)
+
+		# new_animal = Animal.new(name, age, gender, species)
+		# puts new_animal
+
+	when "cc"
+		puts "Enter a client name"
+		name = gets.chomp
+		puts "Enter a client age"
+		age = gets.chomp
+		puts "Enter a client gender"
+		gender = gets.chomp
+		puts "Enter the number of children"
+		number_of_children = gets.chomp
+		$shelter.create_a_client(name, age, gender, number_of_children)
+	 	# new_client = Client.new(name, age, gender, number_of_children)
+		puts new_client
+
+	when "aa"
+		 # puts "what client wants adopt"
+		 #  client_adopt = gets.chomp
+		 # puts "What pet will be adopted?"
+		 # 	pet_adopted = gets.chomp
+
+		 $shelter.adopt  
+
+	when "ra"
+		$shelter.return
+
+
+
+ end
+
+
+
+
+  puts "Please choose one of the following options:
+		(da) display all animals
+		(dc) display all clients
+		(ca) Create an animal
+		(cc) Create a client
+		(aa) adopt an animal
+		(ra) return an animal
+		(q) quit program"
+  	response = gets.chomp.downcase
+  	#set this equal to something
 end
--- shelter.rb
+++ shelter.rb
@@ -1,26 +1,76 @@
+class Shelter 
+	attr_accessor :clients, :animals, :num_animals, :num_clients
 
-require_relative 'client'
-require_relative 'animal'
+	def initialize(name, address)
+		@name = name
+		@address = address
+		@animals= {}	
+		@clients = {}
+		@num_animals = @animals.length
+		@num_clients = @clients.length
+		
+	end
 
-class Shelter
-    attr_accessor :name, :address, :clients, :animals
-   def initialize(name, address)
-       @name = name
-       @address = address
-       @clients = {}
-       @animals ={}
-    end
-    def to_s
-    "#{@name} shelter at #{@address} has #{@animals.length} animals and #{@clients.length} people"
-    end
-    
-     def display_clients
-            clients= ""
-		    @clients.each do |k, v|
-		 	  clients += (v.to_s + "\n")
-		     end
-		     return clients.chomp() 
-		    end
- 
-    
- end
+	def to_s
+		"#{@name} shelter at #{@address} has #{@num_animals} animals and #{@clients.length} people"
+	end
+
+	
+	def display_clients
+	  clint = Clients.to_s
+	  puts clint
+    	#@clients.each do |client| 
+         ##@num_clients = client
+      # end
+       #@num_clients
+	#end
+       
+	def display_pets
+		#@animals.each_key do |animal|
+		animal = Clients.display_pets
+			puts animal
+		end
+	end
+
+	def create_a_client(name, age, gender, number_of_children)
+		@clients[name] = Client.new(name, age, gender, number_of_children)
+	end
+
+	def create_a_animal(name, age, gender, species)
+		@animals[name] = Animal.new(name, age, gender, species)
+	end
+
+	def adopt
+		puts "What client wants to adopt?"
+		client_adopting = gets.chomp.to_sym
+
+		puts "What pet does client want to adopt?"
+		pet_adopted = gets.chomp
+
+		pets["client_adopting"] = pet_adopted
+	end
+# 	def adopt 
+
+# 		puts "what client wants adopt"
+# 			 client_adopt = gets.chomp
+# 	 puts "What pet will be adopted?"
+# 	  	pet_adopted = gets.chomp
+
+# 	  	pet_adopted.push @client.pet 
+# 	  	# create has with client, aniaml  and have it add a pair to adopt
+# send pet into clients pets hash
+
+	# end
+	def return
+		puts "What client is returning a pet?"
+		client_returning = gets.chomp.to_sym
+
+		puts "What pet does client want to return?"
+		pet_return = gets.chomp
+
+		pets.delete(:client_returning)
+		#pets[:client_adopting] = pet_adopted
+		# create same client animal has and have it set value to 0 or eject
+	end
+end
+
